2920caeb98964aa379cda0c22351f02dd61dc9e0
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2022-11-19 13:02:12 +0600

    my-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Adnan Hassan';
+const ocupation = "Programmer";
+const details = ['Adnan Hassan', "Programmer", 'Fuska Developer']
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hello React</h2>
+        <div className="container">
+          <h2>Mui React Shikhte Aisi</h2>
+        </div>
+        <div className="developer">
+          <h3>Name : {details[0]}, Ocupation: {details[1]}</h3>
+        </div>
       </header>
     </div>
   );
